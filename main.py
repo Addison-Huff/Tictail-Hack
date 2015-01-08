04e07662a2ee71eb7f222b3682f12f5103f90d5e
addison.huff@gmail.com <addison.huff@newscred.com>
2015-01-07 21:29:48 -0500

    Set up Mongodb integration
    Requirements.txt was updated with this commit, so remember to `pip
    install`. A basic route was also set up (/todos) which retrieves a list
    of all todo documents from mongo.

--- main.py
+++ main.py
@@ -2,14 +2,24 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from flask.ext.pymongo import PyMongo
+from bson.json_util import dumps
 from contextlib import closing
 
 # create the app
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'todo_app'
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/todos')
+def get_all_todos():
+    results = mongo.db.todos.find()
+    return dumps(results)
 
 if __name__ == "__main__":
     app.run()
