427994dfc9509d9250b630f4e61819ac24b35ffc
addison.huff@gmail.com <addison.huff@newscred.com>
2015-01-08 01:20:06 -0500

    Wire together React template and Flask REST API
    jQuery was included for easy ajax requests (I used their
    CDN). Most work in this commit was done to give the Todo List a reactive
    state, as well as to handle POST requests for new todo items.

--- static/todoapp.js
+++ static/todoapp.js
@@ -1,12 +1,44 @@
 "use strict";
 
 var TodoBox = React.createClass({
+  loadTodosFromServer: function() {
+    $.ajax({
+      url: "/todos",
+      dataType: "json",
+      success: function(data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleTodoSubmit: function(todo) {
+    var todos = this.state.data;
+    var newTodos = todos.concat([todo]);
+    this.setState({ data: newTodos });
+    $.ajax({
+      type: "POST",
+      url: "/todo",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(todo),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return { data: [] };
+  },
+  componentDidMount: function() {
+    this.loadTodosFromServer();
+  },
   render: function() {
     return (
       <div className="todoBox">
         <h1>Todos</h1>
-        <TodoForm />
-        <TodoList />
+        <TodoForm onTodoSubmit={ this.handleTodoSubmit } />
+        <TodoList data={ this.state.data } />
       </div>
     );
   }
@@ -14,16 +46,32 @@ var TodoBox = React.createClass({
 
 var TodoList = React.createClass({
   render: function() {
+    var todoItems = this.props.data.map(function (todo) {
+      return (
+        <Todo completed={ todo.completed }>
+          { todo.text }
+        </Todo>
+      );
+    });
     return (
       <div className="todoList">
-        <Todo> Buy Milk </Todo>
-        <Todo> Buy Bread </Todo>
+        { todoItems }
       </div>
     );
   }
 });
 
 var TodoForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text) {
+      return;
+    }
+    this.props.onTodoSubmit({ text: text, completed: false });
+    this.refs.text.getDOMNode().value = "";
+    return;
+  },
   render: function() {
     return (
       <form className="todoForm" onSubmit={ this.handleSubmit }>
@@ -48,6 +96,6 @@ var Todo = React.createClass({
 });
 
 React.render(
-  <TodoBox url="todos.json" />,
+  <TodoBox />,
   document.getElementById("content")
 );
