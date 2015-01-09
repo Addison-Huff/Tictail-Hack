b92bcb07415cf96612b2e7771835fdaea70d8282
addison.huff@gmail.com <addison.huff@newscred.com>
2015-01-09 01:50:52 -0500

    Set up TodoFooter React class
    This commit has several important changes:
    1. A `TodoFooter` React class was created, which includes both a count
    of remaining todos as well as a function to complete all todos.
    2. In order to properly update the count whenever a todo is completed
    (or uncompleted), the parent TodoBox's state has to update. This
    requires a method TodoBox which is invoked from the completed Todo item.
    3. Additionally, Todo items weren't updating in the DOM when the "Mark
    all as complete" link was clicked. This was because the `completed`
    attribute on a Todo item is passed down from `TodoBox.data` as a
    property, which doesn't invoke a state change. The solution to this is
    to override the `componentWillReceiveProps` method and manually set the
    state, which will re-render the Todo component if the state has changed.
    
    Some other small changes in this commit include styling for the
    TodoFooter and a small HTML fix to include underscore.min.js.

--- static/todoapp.js
+++ static/todoapp.js
@@ -31,10 +31,29 @@ var TodoBox = React.createClass({
       }.bind(this)
     });
   },
+  handleTodoCompletion: function(todoId) {
+    var todo = _.find(this.state.data, function(todo) { return todo._id.$oid === todoId });
+    var newTodos = _.without(this.state.data, todo);
+    todo.completed = todo.completed == true ? false: true;
+    newTodos.splice(todo.position, 0, todo);
+    this.setState({ data: newTodos });
+  },
   reorderTodos: function(todo, newPos) {
     var todoToMove = this.state.data.splice(todo.position, 1)[0];
     var reorderedTodos = this.state.data.splice(newPos, 0, todoToMove);
     this.setState({ data: reorderedTodos });
+  },
+  completeAllTodos: function(e) {
+    e.preventDefault();
+    var incompleteTodos = _.where(this.state.data, { completed: false });
+    var completedTodos = _.difference(this.state.data, incompleteTodos);
+    _.each(incompleteTodos, function(todo) {
+      this.handleTodoCompletion(todo._id.$oid);
+      $.post("/todo/" + todo._id.$oid + "/complete");
+    }, this);
+    var newTodos = _.union(incompleteTodos, completedTodos);
+    var orderedTodos = _.sortBy(newTodos, function (todo) { return todo.position; });
+    this.setState({ data: orderedTodos });
   },
   getInitialState: function() {
     return { data: [] };
@@ -43,11 +62,13 @@ var TodoBox = React.createClass({
     this.loadTodosFromServer();
   },
   render: function() {
+    var numLeft = _.where(this.state.data, { completed: false }).length;
     return (
       <div className="todoBox">
         <h1>Todos</h1>
         <TodoForm onTodoSubmit={ this.handleTodoSubmit } />
-        <TodoList data={ this.state.data } />
+        <TodoList handleTodoCompletion={ this.handleTodoCompletion } data={ this.state.data } />
+        <TodoFooter numLeft={ numLeft } completeAll={ this.completeAllTodos }/>
       </div>
     );
   }
@@ -76,10 +97,18 @@ var TodoForm = React.createClass({
 
 var TodoList = React.createClass({
   render: function() {
+    var handleTodoCompletion = this.props.handleTodoCompletion;
     var todoItems = this.props.data.map(function (todo) {
-      var even = todo.position % 2 !== 0; // I'm naming it "even" based off of list position, not list index
+      var even = todo.position % 2 !== 0; // "Even" is based on actual list position, not list index
+      var props = {
+        handleTodoCompletion: handleTodoCompletion,
+        key: todo._id.$oid,
+        completed: todo.completed,
+        even: even,
+        reactId: todo._id.$oid
+      };
       return (
-        <Todo key={ todo._id.$oid } completed={ todo.completed } even={ even } reactId={ todo._id.$oid }>
+        <Todo {...props} >
           { todo.text }
         </Todo>
       );
@@ -93,12 +122,16 @@ var TodoList = React.createClass({
 });
 
 var Todo = React.createClass({
-  getInitialState: function () {
+  getInitialState: function() {
     return { completed: this.props.completed };
   },
-  handleChange: function () {
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({ completed: nextProps.completed });
+  },
+  completeTodo: function() {
     var completed = this.state.completed == true ? false: true;
     this.setState({ completed: completed });
+    this.props.handleTodoCompletion(this.props.reactId);
     $.post("/todo/" + this.props.reactId + "/complete");
   },
   render: function() {
@@ -112,10 +145,21 @@ var Todo = React.createClass({
     return (
       <div className={ classString }>
         <form className="checkbox">
-          <input type="checkbox" name="completed" checked={ this.state.completed } id={ this.props.reactId } onChange={ this.handleChange }/>
+          <input type="checkbox" name="completed" checked={ this.state.completed } id={ this.props.reactId } onChange={ this.completeTodo }/>
           <label htmlFor={ this.props.reactId }></label>
         </form>
         { this.props.children }
+      </div>
+    );
+  }
+});
+
+var TodoFooter = React.createClass({
+  render: function() {
+    return (
+      <div className="todoFooter">
+        <span className="todoCounter">{ this.props.numLeft } items left</span>
+        <a className="completeAll" href="#" onClick={ this.props.completeAll }>Mark all as complete</a>
       </div>
     );
   }
